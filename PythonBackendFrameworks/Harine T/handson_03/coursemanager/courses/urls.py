903043f628022a04a7b2d638469211de8cdcec42
harcodee <tharine28@gmail.com>
2026-06-30 12:47:01 +0530

    Complete Hands-On 3 Task 1 - DRF APIView CRUD

---
+++ PythonBackendFrameworks/Harine T/handson_03/coursemanager/courses/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import CourseListView, CourseDetailView
+
+urlpatterns = [
+
+    path(
+        "courses/",
+        CourseListView.as_view(),
+    ),
+
+    path(
+        "courses/<int:pk>/",
+        CourseDetailView.as_view(),
+    ),
+
+]
